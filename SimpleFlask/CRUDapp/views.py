66ed47e411d62b2b7c002333bf8501d76548f41d
Sparrow1029 <arayart88@gmail.com>
2017-11-21 01:12:11 -0500

    More things to do... Starting to form the OpenID login. Need to look into OAuth and OAuth2

--- SimpleFlask/CRUDapp/views.py
+++ SimpleFlask/CRUDapp/views.py
@@ -30,12 +30,16 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
         flash('Login requested for OpenID="%s", remember_me=%s' %
               (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
     return render_template('login.html',
                            title='Sign In',
                            form=form,
