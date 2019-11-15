cc8aced582325b0ea59f848be23d88c7dbd2927c
Alex Ray <c-Alexander.Ray@charter.com>
2019-11-15 16:10:06 -0700

    Creating chat server/client

---
+++ chat-app/chatapp.py
@@ -0,0 +1,9 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+# to use buttons:
+from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
+import socket_client
